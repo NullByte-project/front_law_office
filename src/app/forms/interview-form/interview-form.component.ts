ccee7c33ba048cceadbf4906194b8059ec447a69
Juan pablo Osorno <jpog0411@gmail.com>
2025-05-05 23:48:00 -0500

    Corrección de barbaridades que habia hecho mauricio

--- src/app/forms/interview-form/interview-form.component.ts
+++ src/app/forms/interview-form/interview-form.component.ts
@@ -23,8 +23,6 @@ import { RadioButtonModule } from 'primeng/radiobutton';
   styleUrl: './interview-form.component.scss'
 })
 export class InterviewFormComponent {
- 
-  formSubmitted = false;
 
   interview = {
     email: '',
@@ -54,87 +52,9 @@ export class InterviewFormComponent {
   
 
   constructor(private alertService: AlertService, private clientService: ClientService, private interviewService: InterviewService,private router: Router) {}
- 
- 
-  documentTypes = [
-    'Cédula de Ciudadanía',
-    'Cédula de Extranjería',
-    'Tarjeta de Identidad',
-    'Pasaporte',
-    'Otro'
-  ];
-
 
   actions = ['Asesoría', 'Recepción', 'Documento de turno'];
-
-
-  step = 1;
-
-  
-  ageRanges = [
-    'Menor de 18 años (N.N.A)',
-    'De 18-28 Años',
-    'De 29-65 Años',
-    'Mayor de 65 años (Genonte o por Añocidad)'
-  ];
-  
-  residenceOptions = ['Manizales', 'Villamaría', 'Otro'];
-  
-
-  nextStep(currentStep: number) {
-    this.formSubmitted = true;
-  
-    if (!this.validateStep(currentStep)) {
-      this.alertService.error('Por favor complete todos los campos requeridos.');
-      return;
-    }
-  
-    this.formSubmitted = false;
-    this.step = currentStep + 1;
-  }
-  
-  validateStep(step: number): boolean {
-    switch (step) {
-      case 1:
-        return !!(
-          this.interview.email &&
-          this.interview.fullName &&
-          this.interview.documentType &&
-          this.interview.documentNumber
-        );
-  
-      case 2:
-        return !!(
-          this.interview.ageRange &&
-          this.interview.phone &&
-          this.interview.altEmail &&
-          this.interview.address &&
-          this.interview.residence
-        );
-  
-      // Paso 3 a 10: se implementarán más adelante
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case 10:
-        return true;
-  
-      default:
-        return false;
-    }
-  }
-  
-  previousStep() {
-    if (this.step > 1) {
-      this.step--;
-      this.formSubmitted = false; // Limpiar validaciones al retroceder
-    }
-  }
-  
+    
   ngOnInit() {
     // Verificar si hay datos en localStorage y cargarlos en el formulario
     //localStorage.removeItem('interviewForm');
