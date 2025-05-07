954fec36e149ef3da9efb3ff392254c7b716b266
Juan pablo Osorno <jpog0411@gmail.com>
2025-05-07 09:37:10 -0500

    Creación de entrevistas finalizado

--- src/app/forms/interview-form/interview-form.component.ts
+++ src/app/forms/interview-form/interview-form.component.ts
@@ -8,9 +8,10 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { TextareaModule } from 'primeng/textarea';
-import { ClientService } from '../../services/client.service';
 import { AlertService } from '../../services/alerts.service';
 import { InterviewService } from '../../services/interview.service';
+import { AreaServiceService } from '../../services/area.service';
+import { ProceduresService } from '../../services/procedures.service';
 import { Router } from '@angular/router';
 import { RadioButtonModule } from 'primeng/radiobutton';
 
@@ -49,15 +50,23 @@ export class InterviewFormComponent {
     },
     client: {}
   };
+
+  areas: any[] = []; // Inicializa el array de áreas como vacío
+  procedures: any[] = []; // Inicializa el array de procesoPorArea como vacío
+
+  selectedAreaId: number | null = null; // Variable para almacenar el área seleccionada
   
 
-  constructor(private alertService: AlertService, private clientService: ClientService, private interviewService: InterviewService,private router: Router) {}
+  constructor(private alertService: AlertService, private areaService: AreaServiceService, 
+              private interviewService: InterviewService,private router: Router,
+              private procedureService: ProceduresService) {}
 
   actions = ['Asesoría', 'Recepción', 'Documento de turno'];
     
   ngOnInit() {
     // Verificar si hay datos en localStorage y cargarlos en el formulario
     //localStorage.removeItem('interviewForm');
+    this.getAreas();
     const storedInterview = localStorage.getItem('interviewForm');
     const storedClient = JSON.parse(localStorage.getItem('dataClient') || '{}');
     this.interview.client = storedClient;
@@ -74,4 +83,71 @@ export class InterviewFormComponent {
     localStorage.setItem('interviewForm', interviewData);
     this.router.navigate(['/socioeconomico']);
   }  
+
+  getAreas() {
+    this.areaService.getAllAreas().subscribe({
+      next: (response) => {
+        this.areas = response.map((area: any) => ({
+          label: area.name,
+          value: area.id
+        }));
+        console.log('Áreas para dropdown:', this.areas);
+      },
+      error: (error) => {
+        console.error('Error al obtener las áreas:', error);
+      },
+    });
+  }
+
+  onAreaChange(areaId: number) {
+    this.selectedAreaId = areaId;
+    if (!areaId) {
+      this.procedures = [];
+      return;
+    }
+  
+    this.procedureService.getProceduresByArea(areaId).subscribe({
+      next: (response) => {
+        this.procedures = response.map((procedure: any) => ({
+          label: procedure.name,
+          value: procedure.id // Guardamos el objeto completo aquí
+        }));
+        console.log('Procedimientos para el área seleccionada:', this.procedures);
+      },
+      error: (error) => {
+        console.error('Error al obtener procedimientos:', error);
+      },
+    });
+  }
+
+  submitInterview() {
+    //console.log('Entrevista:', this.interview);
+    this.alertService.loading('Creando entrevista...');
+    this.interviewService.createInterview(this.interview).subscribe({
+      next: (response) => {
+        // Guardar el ID de la entrevista en localStorage
+        this.alertService.success('Entrevista registrada', 'La entrevista ha sido creada exitosamente')
+        .then(() => {
+          localStorage.removeItem('dataClient')
+          localStorage.removeItem('socioForm')
+          localStorage.removeItem('interviewForm');
+          this.router.navigate(['/']);
+        });
+      },
+      error: (err) => {
+        console.error('Error al crear entrevista:', err);
+        this.alertService.error('Error al registrar', 'No se pudo registrar la entrevista.');
+      }
+    });
+  }
+
+  onActionChange(action: string) {
+    if (action !== 'Recepción') {
+      this.interview.legalCase.legalAction.approvalCode = '';
+      this.interview.legalCase.legalAction.procedure = null;
+      this.interview.legalCase.legalAction.instructions = '';
+      this.interview.legalCase.legalAction.additionalInfo = '';
+      this.interview.legalCase.folder = '';
+    }
+  }
 }
