e1389eea41abaa9d837e876c952e2c2b710eb538
Juan pablo Osorno <jpog0411@gmail.com>
2025-05-17 00:50:21 -0500

    Cambio de puerto para los endpoints

--- src/app/config/config.routes.backend.ts
+++ src/app/config/config.routes.backend.ts
@@ -1,9 +1,9 @@
 export namespace ConfigRoutesBackend {
-  export const clients:string    = 'http://localhost:8080/api/clients/';
-  export const interwiews:string = 'http://localhost:8080/api/interviews/';
-  export const areas:string      = 'http://localhost:8080/api/areas/';
-  export const procedures:string = 'http://localhost:8080/api/procedures/';
-  export const cases:string      = 'http://localhost:8080/api/cases/';
-  export const approvalCode:string = 'http://localhost:8080/api/approval-codes/generate';
-  export const legalActions:string = 'http://localhost:8080/api/stages-legal-action/';
+  export const clients:string    = 'http://localhost:8081/api/clients/';
+  export const interwiews:string = 'http://localhost:8081/api/interviews/';
+  export const areas:string      = 'http://localhost:8081/api/areas/';
+  export const procedures:string = 'http://localhost:8081/api/procedures/';
+  export const cases:string      = 'http://localhost:8081/api/cases/';
+  export const approvalCode:string = 'http://localhost:8081/api/approval-codes/generate';
+  export const legalActions:string = 'http://localhost:8081/api/stages-legal-action/';
 }
